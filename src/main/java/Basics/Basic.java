19a391f86e51093167f7901760c3827caee85a0a
Rohit Parihar <rohitparih@gmail.com>
2023-08-22 21:31:39 +0530

    22-August

---
+++ src/main/java/Basics/Basic.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class Basic {
+    public static void main(String[] args){
+
+        //This line will print my name
+        //Single Line Comment
+        /*
+            Multiline Comment
+            Comment Here
+         */
+        System.out.println("Rohit Parihar");
+        System.out.println("Atharva");
+        System.out.println(10);
+    }
+}
---
+++ src/main/java/Basics/Variables.java
@@ -0,0 +1,26 @@
+package Basics;
+
+public class Variables {
+    public static void main(String[] args){
+        //How to declare variables
+        // type <variableName> = value;
+
+        /*
+            String -> It will store text, words, etc.
+		  (Must be surrounded by Double Quotes "Rohit")
+         */
+        String name = "Rohit"; //Variable
+        String college = "Chandigarh University";
+        System.out.println(name);
+        System.out.println(college);
+
+        /*
+                int -> Will store integers or numbers (Not decimal)
+		        (It will store 10 but not 10.15)
+         */
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
