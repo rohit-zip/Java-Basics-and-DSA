36f343a44444a0a4c8e9e0851c5f6faab9df3fbf
Rohit Parihar <rohitparih@gmail.com>
2023-09-15 21:09:04 +0530

    15-September

--- src/main/java/Arrays/TwoSum.java
+++ src/main/java/Arrays/TwoSum.java
@@ -8,14 +8,33 @@ package Arrays;
  */
 
 public class TwoSum {
-    public static void main(String[] args) {
-        int[] array = {2, 11, 7, 15};
-        int target = 9;
 
-        for (int i=1 ; i<array.length ; i++){
-//            System.out.println(array[i] + array[i+1]); //ArrayIndexOutOfBoundsException
-            int sum = array[i-1] + array[i];
-            System.out.println(sum);
+    public static int[] twoSum(int[] array, int target){
+        for (int i=0 ; i<array.length ; i++){
+            int value = array[i];
+            for (int j=i+1 ; j<array.length ; j++){
+                if (value + array[j] == target){
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 11, 7, 15, 16, 10, 25};
+        int target = 27;
+
+//        for (int i=1 ; i<array.length ; i++){
+////            System.out.println(array[i] + array[i+1]); //ArrayIndexOutOfBoundsException
+//            int sum = array[i-1] + array[i];
+//            System.out.println(sum);
+//        }
+
+        int[] resultArray = twoSum(array, target);
+
+        for (int i=0 ; i<resultArray.length ; i++){
+            System.out.println(resultArray[i]);
         }
     }
 }
