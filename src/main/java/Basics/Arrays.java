780d3de2c9f550a47df96e945ddfc072e036627e
Rohit Parihar <rohitparih@gmail.com>
2023-09-18 20:54:21 +0530

    18-September

--- src/main/java/Basics/Arrays.java
+++ src/main/java/Basics/Arrays.java
@@ -12,6 +12,10 @@ public class Arrays {
         newArray[3] = 16;
 
         String[] strings = {"rohit", "atharva", "rakesh", "mukesh"};
+        String str = "My name is Rohit Parihar from Pune";
+
+        //{"My", "name", "is", "Rohit", "Parihar", "from", "Pune"};
+        String[] stringArray = str.split(" ");
 
         /**
          *
@@ -24,31 +28,41 @@ public class Arrays {
 //        System.out.println(array[4]);
 //        System.out.println(array[5]); ArrayIndexOutOfBoundException
 //        System.out.println(array.length);
-        for (int i=0 ; i<array.length ; i++){
-            System.out.println(array[i]);
-        }
-
-        for (int i=array.length - 1 ; i>=0 ; i--){
-            System.out.println(array[i]);
-        }
-
-        //While Loop
+//        for (int i=0 ; i<array.length ; i++){
+//            System.out.println(array[i]);
+//        }
+//
+//        for (int i=array.length - 1 ; i>=0 ; i--){
+//            System.out.println(array[i]);
+//        }
+//
+//        //While Loop
+//
+//        for (int i=0 ; i<strings.length ; i++){
+//            System.out.println(strings[i]);
+//        }
+//
+//        for (int i = 0 ; i<array.length ; i++){
+//            if (array[i]==2){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 0 ; i<array.length ; i++){
+//            if (array[i]%2==0){
+//                System.out.println(i);
+//            }
+//        }
 
         for (int i=0 ; i<strings.length ; i++){
-            System.out.println(strings[i]);
+            String arrayValue = strings[i];
+            System.out.println(arrayValue.toUpperCase());
         }
 
-        for (int i = 0 ; i<array.length ; i++){
-            if (array[i]==2){
-                continue;
-            }
-            System.out.println(i);
-        }
-
-        for (int i = 0 ; i<array.length ; i++){
-            if (array[i]==2){
-                System.out.println(i);
-            }
-        }
+        //Extract all even numbers
+        //Extract all odd Numbers
+        //Index of r in strings array
+        //Find the largest word in String
     }
 }
