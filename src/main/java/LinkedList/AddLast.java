35d30027324027a6d5f4a08098b58a7fcd66b197
rohit <rohitparih@gmail.com>
2023-10-25 20:46:12 +0530

    25-October

--- src/main/java/LinkedList/AddLast.java
+++ src/main/java/LinkedList/AddLast.java
@@ -4,29 +4,58 @@ package LinkedList;
  * @author - rohit
  * @project - Atharva_Tutorial
  * @package - LinkedList
- * @created_on - 10 October-2023
+ * @created_on - 25 October-2023
  */
-
 public class AddLast {
 
-    public static class Node{
+    public static class Node {
         int data;
         Node next;
     }
 
-    public static class CustomLinkedList{
+    public static class CreateLinkedList {
         Node head;
         Node tail;
         int size;
 
-        public void addLast(int value){
+        public void addLast(int data) {
             Node node = new Node();
-            node.data = value;
-            if (size == 0){
-                tail = head = node;
+            node.data = data;
+            node.next = null;
+            if (size==0) {
+                head = tail = node;
             } else {
+                tail.next = node;
+                tail = node;
+            }
+            size++;
+        }
 
+        public int size() {
+            return size;
+        }
+
+        public void display() {
+            for (Node temp = head ; temp != null ; temp = temp.next) {
+                System.out.print(temp.data + " -> ");
             }
         }
+
+        public void printHeadAndTail() {
+            System.out.println("Head : " + head.data);
+            System.out.println("Tail : " + tail.data);
+        }
+    }
+
+    public static void main(String[] args) {
+        CreateLinkedList linkedList = new CreateLinkedList();
+        linkedList.addLast(2);
+        linkedList.addLast(4);
+        linkedList.addLast(5);
+        linkedList.addLast(6);
+        linkedList.addLast(7);
+        linkedList.display();
+        linkedList.printHeadAndTail();
+        //Create a method to add data at first
     }
 }
