d70f45e849bd1a8656f2d08ffe677d181f601876
rohit <rohitparih@gmail.com>
2024-01-17 21:50:36 +0530

    Generic Tree -> `Questions`

--- src/main/java/generictree/GenericTree.java
+++ src/main/java/generictree/GenericTree.java
@@ -8,8 +8,8 @@ import java.util.Stack;
  * Author - rohit
  * Project - Atharva_Tutorial
  * Package - generictree
- * Created_on - 15 January-2024
- * Created_at - 21 : 49
+ * Created_on - 17 January-2024
+ * Created_at - 21 : 20
  */
 
 public class GenericTree {
@@ -19,12 +19,32 @@ public class GenericTree {
         ArrayList<Node> children = new ArrayList<>();
     }
 
+    public static void display(Node root) {
+        String str = root.data + " -> ";
+        for (Node node : root.children) {
+            str += node.data + " , ";
+        }
+        str += " . ";
+        System.out.println(str);
+        for (Node node : root.children) {
+            display(node);
+        }
+    }
+
+    public static int size(Node root) {
+        int s = 0;
+        for (Node child : root.children) {
+            int childSize = size(child);
+            s = s + childSize;
+        }
+        s = s + 1;
+        return s;
+    }
+
     public static void main(String[] args) {
         int[] array = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
-
-        Node root;
         Stack<Node> st = new Stack<>();
-
+        Node root = null;
         for (int i=0 ; i<array.length ; i++) {
             if (array[i] == -1) {
                 st.pop();
@@ -32,12 +52,15 @@ public class GenericTree {
                 Node node = new Node();
                 node.data = array[i];
                 if (st.size() > 0) {
-                    st.peek().children.add(node);
+                    Node peek = st.peek();
+                    peek.children.add(node);
                 } else {
                     root = node;
                 }
                 st.push(node);
             }
         }
+//        display(root);
+        System.out.println(size(root));
     }
 }
