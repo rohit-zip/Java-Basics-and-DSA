a26b3c1bca67f8c2d4a8642f4433b376434b56ee
rohit <rohitparih@gmail.com>
2024-01-23 20:42:14 +0530

    Generic Tree -> Display

---
+++ src/main/java/generictree/Size.java
@@ -0,0 +1,9 @@
+package generictree;/**
+ * Owner - Rohit Parihar
+ * Author - rohit
+ * Project - Atharva_Tutorial
+ * Package - generictree
+ * Created_on - 23 January-2024
+ * Created_at - 20 : 40
+ */public class Size {
+}
